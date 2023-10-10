a51248384e8860fc3da9dd8d4afbf9f62ebe4112
Shola Japheth <sholajapheth@gmail.com>
2023-10-10 11:07:13 +0100

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,11 @@
 "use client";
+import FeedBack from "@/src/components/_Feedback";
 import db from "@/src/configs/db";
 import { doc, setDoc } from "firebase/firestore";
-import Image from "next/image";
 import { useState } from "react";
 import React from "react";
 
-const FeedBack = ({
-  question,
-  handleChange,
-  index,
-}: {
-  question: {
-    question: string;
-    type: string;
-    answer: string;
-  };
-  index: number;
-  handleChange: (e: any, index: number) => void;
-}) => {
-  return (
-    <div
-      className="
-      border-2 border-gray-300 rounded-lg p-2 m-2 w-full
-      dark:border-gray-700 dark:bg-gray-800/30 dark:text-gray-200
-    "
-    >
-      <h3>{question?.question}</h3>
-      <textarea
-        value={question?.answer}
-        onChange={(e) => handleChange(e, index)}
-        placeholder="Type your answer here"
-        className="border-2 border-gray-300 rounded-lg p-2  w-full  text-gray-700 dark:border-gray-700 dark:bg-gray-800/30 dark:text-gray-200"
-        rows={5}
-      ></textarea>
-    </div>
-  );
-};
+
 
 export default function Home() {
   // handle form submission
