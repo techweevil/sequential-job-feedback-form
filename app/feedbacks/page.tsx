f9f968e25cf6961426239cf904444c61dfd6c8f1
Shola Japheth <sholajapheth@gmail.com>
2023-10-10 10:48:26 +0100

    fixes

--- app/feedbacks/page.tsx
+++ app/feedbacks/page.tsx
@@ -43,7 +43,7 @@ const FeedBack = ({
   );
 };
 
-const Feedbacks = () => {
+const Page = () => {
   const [feedbacks, setFeedbacks] = useState([]) as any[];
   const _ref = collection(db, "sequential-job-feedbacks");
 
@@ -76,4 +76,4 @@ const Feedbacks = () => {
   );
 };
 
-export default Feedbacks;
+export default Page;
